7874a306eb4279922a508ac8d954de63289d8e37
aaronagu0604 <690753863@qq.com>
2017-12-26 16:04:31 +0800

    测试

--- handler/admin.py
+++ handler/admin.py
@@ -6,10 +6,22 @@ from lib.route import Route
 
 
 # ----------------------------------------------------------应用页面----------------------------------------------------
-@Route(r'/',name='admin_root')    # 公司官网
+@Route(r'/',name='admin_root')    # 主页
 class RootHandler(BaseHandler):
     def get(self):
         self.render("personal/index.html")
+
+
+@Route(r'/about',name='admin_about')    # 公司官网
+class AboutHandler(BaseHandler):
+    def get(self):
+        self.render("personal/about.html")
+
+
+@Route(r'/post',name='admin_post')    # 公司官网
+class PostHandler(BaseHandler):
+    def get(self):
+        self.render("personal/post.html")
 
 
 @Route(r'/admin/upload_pic', name='admin_upload_pic')    # 图片上传
